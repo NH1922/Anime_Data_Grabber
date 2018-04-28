ac61d882dc377f05a9a27fe3b471f49599d58902
NH1922 <31303415+NH1922@users.noreply.github.com>
2018-04-28 23:13:15 +0530

    Writing To File

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from xml.etree import ElementTree
 import os
 import requests
 import re
@@ -16,32 +15,32 @@ def find_animeid(AnimeName):
     data = links.split("/")
     return(data[4])
 
-def get_anime_data(AnimeID):
+def get_anime_data(AnimeID,path):
     base_url = "https://api.jikan.moe/anime/"
     request_url = base_url +  str(AnimeID)
     response = urllib.request.urlopen(request_url).read()
     response_obj = str(response,'utf-8')
     response_data = json.loads(response_obj)
-    print(response_data['mal_id'])
-    print(response_data['title_english'])
-    #print(response_data['title_japanese'])
-    print(response_data['score'])
-    print(response_data['rating'])
-    print(response_data['status'])
-    print(response_data['popularity'])
-    print(response_data['episodes'])
-    print(response_data['rank'])
-    print(response_data['title'])
-    print(response_data['duration'])
-    print(response_data['genre'])
-
-
-
-
+    file_destination = path + "\AnimeData.txt"
+    file = open(file_destination,"a")
+    file.write("MAL ID:" +str(response_data['mal_id'])  +"\n")
+    file.write("TITLE:" +str(response_data['title'])  +"\n")
+    file.write("SCORE:" +str(response_data['score'])  +"\n")
+    file.write("RATING" +str(response_data['rating'])  +"\n")
+    file.write("STATUS:" +str(response_data['status'])  +"\n")
+    file.write("POPULARITY:" +str(response_data['popularity'])  +"\n")
+    file.write("EPISODES:" +str(response_data['episodes'])  +"\n")
+    file.write("RANK:" +str(response_data['rank'])  +"\n")
+    file.write("DURATION:" +str(response_data['duration'])  +"\n")
+    file.write("GENRE" +str(response_data['genre'])  +"\n")
+    file.write("MAL ID:" +str(response_data['mal_id'])  +"\n")
+    file.write("\n ----- END OF "+response_data['title']+"-----\n\n\n")
 
 if __name__=="__main__":
     path = input("Enter the path to your animes : ")
     animes = next(os.walk(path))[1]
     for anime in animes:
+        print("Adding ",anime)
         AnimeID = find_animeid(anime)
-        get_anime_data(AnimeID)
+        get_anime_data(AnimeID,path)
+    print("SUCCESSFULLY DONE ! AnimeData.txt created in your anime folder")
